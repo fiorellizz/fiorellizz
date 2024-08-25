f724c65f3fbfe387cbdeb6c9b2a0c9c27203a8c3
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2024-08-25 12:52:22 -0300

    Update index.html and script.js

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
-    const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
-    if (elements.length > instance) {
-        // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
-    }
-}
+// Se você precisar de rolagem suave, pode adicionar o comportamento diretamente no CSS.
+document.querySelector('html').style.scrollBehavior = 'smooth';
 
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
+// Adiciona comportamento personalizado, se necessário
+document.addEventListener('DOMContentLoaded', () => {
+    const link1 = document.getElementById("link1");
+    const link2 = document.getElementById("link2");
+    const link3 = document.getElementById("link3");
 
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
-});
+    link1.addEventListener('click', () => {
+        document.querySelector('#habilidades').scrollIntoView({ behavior: 'smooth' });
+    });
 
-link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
-});
+    link2.addEventListener('click', () => {
+        document.querySelector('#projetos').scrollIntoView({ behavior: 'smooth' });
+    });
 
-link3.addEventListener('click', () => {
-    scrollToElement('.column');
+    link3.addEventListener('click', () => {
+        document.querySelector('#sobre').scrollIntoView({ behavior: 'smooth' });
+    });
 });
