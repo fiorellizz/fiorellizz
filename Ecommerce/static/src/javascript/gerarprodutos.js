f359e926acd0216da54464834991bf47fb64cd29
Guilherme Fiorelli Braz Rodrigues <fiorelligui@gmail.com>
2024-10-01 16:29:04 -0300

    v3

---
+++ Ecommerce/static/src/javascript/gerarprodutos.js
@@ -0,0 +1,389 @@
+const produtos = {
+    "Diversos": [
+        {
+            nome: "Fone de Ouvido JBL",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/FoneOuvidoJBL.png",
+            preco: "4R$ 199,00",
+            quantidade: 5,
+            avaliacao: 100
+        },
+        {
+            nome: "Carregador Portátil",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/CarregadorPortatil.png",
+            preco: "4R$ 159,00",
+            quantidade: 4,
+            avaliacao: 100
+        },
+        {
+            nome: "Caixa de Som JBL",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/CaixaDeSomJBL.png",
+            preco: "4R$ 349,00",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Smartwatch Xiaomi",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/SmartwatchXiaomi.png",
+            preco: "4R$ 279,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "TV Smart 4K",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/TvSmart43.png",
+            preco: "4R$ 3.599,00",
+            quantidade: 3,
+            avaliacao: 100
+        },
+        {
+            nome: "Notebook Dell 15",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/NotebookDellInspiron15.png",
+            preco: "4R$ 3.599,00",
+            quantidade: 1,
+            avaliacao: 100
+        },
+        {
+            nome: "Echo Dot",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/EchoDot.png",
+            preco: "4R$ 389,00",
+            quantidade: 4,
+            avaliacao: 100
+        },
+        {
+            nome: "Máquina de Café",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/MaquinaDeCafe.png",
+            preco: "4R$ 399,00",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Airfryer Mondial",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/FritadeiraAirfryer.png",
+            preco: "4R$ 399,00",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "AirPods Apple",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/AirPodsApple.png",
+            preco: "4R$ 1.199,00",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Smartwatch Samsung",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/SmartwatchSamsungGalaxyWatch4.png",
+            preco: "4R$ 1.499,00",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Kindle Paperwhite",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/KindlePaperwhite.png",
+            preco: "4R$ 649,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Fone de Ouvido JBL",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/FoneJBL510BT.png",
+            preco: "4R$ 249,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Caixa Boombox 3",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/CaixaDeSomJBLBoombox3.png",
+            preco: "4R$ 3.299,00",
+            quantidade: 3,
+            avaliacao: 100
+        },
+        {
+            nome: "Caixa Charge 5",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/CaixaDeSomJBLCharge5.png",
+            preco: "4R$ 999,00",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Smart TV LED 4K",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/SmartTVLED.png",
+            preco: "4R$ 2.999,00",
+            quantidade: 1,
+            avaliacao: 100
+        },
+        {
+            nome: "Drone DJI Mini 2",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/DroneDJIMini.png",
+            preco: "4R$ 4.999,00",
+            quantidade: 1,
+            avaliacao: 100
+        },
+        {
+            nome: "Mochila de Viagem",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/MochiladeViagem.png",
+            preco: "4R$ 599,90",
+            quantidade: 1,
+            avaliacao: 100
+        },
+    ],
+    "Automotivo": [
+        {
+            nome: "Kit de Ferramentas",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/KitdeFerramentas.png",
+            preco: "4R$ 399,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Capa de Banco",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/CapadeBanco.png",
+            preco: "4R$ 99,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Kit de Limpeza",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/KitdeLimpeza.png",
+            preco: "4R$ 129,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Bomba de Ar",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/BombadeAr.png",
+            preco: "4R$ 149,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Suporte para Celular",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/SuporteparaCelular.png",
+            preco: "4R$ 39,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Rádio Automotivo",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/RadioAutomotivo.png",
+            preco: "4R$ 349,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Trava de Volante",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/TravadeVolante.png",
+            preco: "4R$ 79,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+    ],
+    "Bebidas":[
+        {
+            nome: "Cerveja Heineken",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/CaixaCervejaHeineken.png",
+            preco: "4R$ 119,90",
+            quantidade: 3,
+            avaliacao: 100
+        },
+        {
+            nome: "Vinho Tinto Chileno",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/VinhoTintoChileno.png",
+            preco: "4R$ 139,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Whisky Johnnie Walker",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/WhiskyJohnnie.png",
+            preco: "4R$ 179,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Vodka Absolut",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/VodkaAbsolut.png",
+            preco: "4R$ 199,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Cafe em Capsulas",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/CafeemCapsulas.png",
+            preco: "4R$ 149,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Energetico Red Bull",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/EnergeticoRedBull.png",
+            preco: "4R$ 54,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+    ],
+    "Beleza e Maquiagem":[
+        {
+            nome: "Base Liquida Matte",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/BaseLiquidaMatte.png",
+            preco: "4R$ 29,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Paleta de Sombras Nude",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/PaletadeSombrasNude.png",
+            preco: "4R$ 99,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Mascara de Cilios",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/MascaradeCilios.png",
+            preco: "4R$ 49,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Batom Liquido Matte",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/BatonLiquidoMatte.png",
+            preco: "4R$ 33,00",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Corretivo Liquido",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/CorretivoLiquido.png",
+            preco: "4R$ 60,00",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Blush Compacto",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/BlushCompacto.png",
+            preco: "4R$ 22,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Iluminador Facial",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/IluminadorFacial.png",
+            preco: "4R$ 25,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Delineador Liquido",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/DelineadorLiquido.png",
+            preco: "4R$ 23,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+        {
+            nome: "Po Compacto",
+            descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+            imagem: "static/src/images/PoCompacto.png",
+            preco: "4R$ 21,90",
+            quantidade: 2,
+            avaliacao: 100
+        },
+    ],
+    "Em Breve":[
+        {
+            nome: "",
+            descricao: "",
+            imagem: "",
+            preco: "",
+            quantidade: 2,
+            avaliacao: 10
+        }
+    ]
+};
+
+function gerarProdutos() {
+    const container = document.getElementById('product-sections');
+    
+    Object.keys(produtos).forEach(categoria => {
+        // Criar título da categoria
+        const categoriaTitulo = document.createElement('h3');
+        categoriaTitulo.classList.add('section-subtitle-products');
+        categoriaTitulo.innerText = categoria;
+        container.appendChild(categoriaTitulo);
+        
+        // Criar o contêiner de produtos
+        const categoriaContainer = document.createElement('div');
+        categoriaContainer.id = "dishes";
+        
+        // Criar os produtos
+        produtos[categoria].forEach(produto => {
+            const produtoDiv = document.createElement('div');
+            produtoDiv.classList.add('dish');
+            
+            produtoDiv.innerHTML = `
+                <div class="dish-heart">
+                    <i class="fa-solid fa-heart"></i>
+                </div>
+                <img src="${produto.imagem}" class="dish-image" alt="${produto.nome}">
+                <h3 class="dish-title">${produto.nome}</h3>
+                <span class="dish-description">${produto.descricao}</span>
+                <div class="dish-quantity">Quantidade disponível: <span>${produto.quantidade}</span></div>
+                <div class="dish-rate">
+                    ${'<i class="fa-solid fa-star"></i>'.repeat(5)}
+                    <span>(${produto.avaliacao}+)</span>
+                </div>
+                <div class="dish-price">
+                    <h4>${produto.preco}</h4>
+                    <button class="btn-default">
+                        <i class="fa-solid fa-basket-shopping"></i>
+                    </button>
+                </div>
+            `;
+            
+            categoriaContainer.appendChild(produtoDiv);
+        });
+        
+        container.appendChild(categoriaContainer);
+    });
+}
+
+// Chamar a função para gerar os produtos na página
+gerarProdutos();
